e6579223f2f47b2924b016244239ba0898d3ed03
Hari <hariprasath.m2024cse@sece.ac.in>
2026-07-26 22:31:07 +0530

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="m-10 p-6 max-w-sm mx-auto bg-slate-600 rounded-xl shadow-lg flex items-center space-x-4">
+      <div className="text-xl font-medium text-white underline">
+        Karghadhan
+      </div>
+    </div>
+  )
+}
+
+export default App
